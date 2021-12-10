ec3fa04543d954c43430c599fba3211ffb93cc6e
MZabell <94442320+MZabell@users.noreply.github.com>
2021-12-10 10:42:39 +0100

    Update potentiometer_with_buffer.ino

--- potentiometer_with_buffer.ino
+++ potentiometer_with_buffer.ino
@@ -1,4 +1,6 @@
-unsigned char buffer[4];
+unsigned char buffer[5];
+// Frequency (Hz)
+int frequency = 100;
 
 void setup() {
   pinMode(4,OUTPUT);
@@ -8,20 +10,17 @@ void setup() {
 }
 
 void loop() {
-  int sensorvalue=analogRead(A0);
-  createWriteBuffer(calculate(sensorvalue), buffer);
-  Serial.write(buffer, 4);
+  int sensorValue = analogRead(A0);
+  float value = sensorValue * (5.0 / 1023.0);
+  createWriteBuffer(value, buffer);
+  Serial.write(buffer, sizeof(buffer));
+  delay(1000 * (1.0/frequency));
 }
 
-float calculate(int sens){
-  float Volt=sens*(5.0/1023.0);
-  return Volt;
-  }
-
 void createWriteBuffer(float value, unsigned char *buffer) {
-  int data = * (int *) &value;
-  
-  for (int i = 0; i < 4; i++) {
-    *(buffer + i) = data >> (24 - 8 * i) & 0xff;
+  long data = * (long *) &value;
+  buffer[0]='<';
+  for (int i = 1; i < 5; i++) {
+    *(buffer + i) = data >> (24 - 8 * (i-1)) & 0xff;
   }
 }
